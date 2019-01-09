aa81d16a15661315fbf6fac01ce979e27c5f3047
Knickmeyer <eknickme@cgifederal.com>
2019-01-09 15:53:22 -0500

    edit

--- ex5.py
+++ ex5.py
@@ -9,24 +9,24 @@ Ex5: More Variables and Printing
 '''
 
 
-my_name = 'Eric J. Knickmeyer'
-my_age = 23 #Jordan year
-my_height = 72 # inches...more or less
-my_weight = 160 # lbs
-my_eyes = 'Blue'
-my_teeth = 'White'
-my_hair = 'Brown'
+# my_name = 'Eric J. Knickmeyer'
+# my_age = 23 #Jordan year
+# my_height = 72 # inches...more or less
+# my_weight = 160 # lbs
+# my_eyes = 'Blue'
+# my_teeth = 'White'
+# my_hair = 'Brown'
 
-print "Let's talk about %s." % my_name
-print "He's %d inches tall." % my_height
-print "He's %d pounds heavy." % my_weight
-print "Actually that's not too heavy."
-print "He's got %s eyes and %s hair." %(my_eyes, my_hair)
-print "His teeth are usually %s depending on the coffee." %my_hair
+# print "Let's talk about %s." % my_name
+# print "He's %d inches tall." % my_height
+# print "He's %d pounds heavy." % my_weight
+# print "Actually that's not too heavy."
+# print "He's got %s eyes and %s hair." %(my_eyes, my_hair)
+# print "His teeth are usually %s depending on the coffee." %my_hair
 
-#tricky line of code... I made a mistake.
-print "if I add %d, %d, and %d I get %d." % (# <-- before this was where my mistake was!!!!
-my_age, my_height, my_weight, my_age + my_height + my_weight)
+# #tricky line of code... I made a mistake.
+# print "if I add %d, %d, and %d I get %d." % (# <-- before this was where my mistake was!!!!
+# my_age, my_height, my_weight, my_age + my_height + my_weight)
 
 # ---------------------------------------------------------------------------
 
